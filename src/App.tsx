97532592369dedeaa2fdb0f590ef6e9cc6b299cf
cleberson-dev <cleberson.dev@gmail.com>
2020-08-05 01:40:24 -0300

    WIP: Building main section

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import Header from './components/Header';
 import './App.scss';
 import polygonImg from './img/polygon.png';
+import illustration from './img/illustration.svg';
+import SearchBar from './components/SearchBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="intro">
+        <div className="left">
+          <p>Buscar um usuário no <strong>Github</strong><br/> ficou muito <strong>fácil!</strong></p>
+          <SearchBar />
+        </div>
+          <img src={illustration} alt="Illustration" />
+      </div>
+      <Footer />      
+
+
       <img className="polygon" src={polygonImg} />
     </div>
   );
