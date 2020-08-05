adbf9f14adb5a75aa249789cea672c8ba2985926
cleberson-dev <cleberson.dev@gmail.com>
2020-08-05 00:29:01 -0300

    Implemented Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.scss';
+import polygonImg from './img/polygon.png';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="App-title">Hello, world!</h1>
+      <Header />
+      <img className="polygon" src={polygonImg} />
     </div>
   );
 }
