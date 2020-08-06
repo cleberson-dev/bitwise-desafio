0b041d9efcc653bf6b807178985321bb1da40ffb
cleberson-dev <cleberson.dev@gmail.com>
2020-08-06 15:35:25 -0300

    Added SearchBar and Social components to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import logo from '../../img/logo.png';
+import { useLocation } from 'react-router-dom';
 import './styles.scss';
+import SearchBar from '../SearchBar';
+import Socials from '../Socials';
+import logo from '../../img/logo.png';
+import instagram from "../../img/social/instagram.svg";
+import linkedin from "../../img/social/linkedin.svg";
+import medium from "../../img/social/medium.svg";
+import Social from '../../interfaces/Social.interface';
 
 function Header() {
+  const location = useLocation();
+  const { pathname: currentPath } = location;
+
+  const socials: Social[] = [
+    { name: "Instagram", link: "#", iconUrl: instagram },
+    { name: "Linkedin", link: "#", iconUrl: linkedin },
+    { name: "Medium", link: "#", iconUrl: medium },
+  ];
+
   return (
     <header>
       <img src={logo} alt="Bitwise logo" />
+      { currentPath !== '/' && (
+        <SearchBar hideButton searchHandler={alert} />
+      )}
+      <Socials socials={socials} />
     </header>
   );
 }
