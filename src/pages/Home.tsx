69c33537737dcfe20c4568de6b856c6e1010d546
cleberson-dev <cleberson.dev@gmail.com>
2020-08-06 15:36:51 -0300

    Adjusted styling on Home and SearchResult components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,17 +8,17 @@ function HomePage() {
   return (
     <>
       <main>
+        <img className='polygon' src={polygonImg} />
         <div className='intro'>
           <div className='left'>
             <p>
               Buscar um usuário no <strong>Github</strong>
               <br /> ficou muito <strong>fácil!</strong>
             </p>
-            <SearchBar />
+            <SearchBar searchHandler={alert} />
           </div>
           <img src={illustration} alt='Illustration' />
         </div>
-        <img className='polygon' src={polygonImg} />
       </main>
 
       <Footer />
